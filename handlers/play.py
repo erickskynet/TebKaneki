a4d34c87016fca83cc305e4482ad06bebfc68928
SKY <erickskynet@users.noreply.github.com>
2021-10-16 18:56:58 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -25,6 +25,7 @@ from config import (
 from downloaders import youtube
 from helpers.admins import get_administrators
 from helpers.channelmusic import get_chat_id
+from helpers.chattitle import CHAT_TITLE
 from helpers.decorators import authorized_users_only
 from helpers.filters import command, other_filters
 from helpers.gets import get_file_name
